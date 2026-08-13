4508e72057a53822e8363ba39d21d292e35a3fa8
bibixx-pixel <db.jung@oneauto.co.kr>
2026-08-13 17:34:02 +0900

    Feat: PLC 통신 자동 재연결 로직 구현

--- src/plc_worker.py
+++ src/plc_worker.py
@@ -20,6 +20,7 @@ class Worker(QThread):
 
 	def __init__(self, ip, port):
 		super().__init__()
+		# 스레드의 동작 상태(ON/OFF)를 제어하기 위해 만든 플래그 변수 (스위치 역할함)
 		self.power = True
 		self.ip = ip
 		self.port = port
@@ -100,138 +101,153 @@ class Worker(QThread):
 			print(f"읽기 오류 발생: {ex}")
 
 	def run(self):
-		client_socket = MySocket()
-
 		with open('src/devices.json', 'r') as f:
 			device_data = json.load(f)
 
 		temp_read = device_data['plc_d_register']['temp_read']
 
-		try:
-			# 서버 소켓에 접속
-			client_socket.connect(self.ip, self.port) #temp_read
-			self.communication_signal.emit("통신 연결됨")
+		# self.power가 True인 동안(프로그램 실행 중) 계속 재연결 시도
+		while self.power:
+			client_socket = MySocket()
 
-			count = 0
-			while self.power:
-				# PLC 데이터 읽기
-				# 1초 주기 가상 온도 데이터 생성 (10.0 ~ 100.0 범위의 난수, 실수 표현위해 uniform 사용)
-				temp_data = random.uniform(10.0, 100.0)
+			try:
+				# 서버 소켓에 접속
+				client_socket.connect(self.ip, self.port) #temp_read
+				self.communication_signal.emit("통신 연결됨")
 
-				# 난수를 D 레지스터에 쓰기 테스트
-				try:
-					success = client_socket.mc_write_d_word(temp_read, int(temp_data * 10))
+				count = 0
+				
+				# 통신이 성공한 상태에서 데이터 수집 루프
+				while self.power:
+					# 1초 주기 가상 온도 데이터 생성 (10.0 ~ 100.0 범위의 난수, 실수 표현위해 uniform 사용)
+					temp_data = random.uniform(10.0, 100.0)
 
-					# 만약 쓰는데 실패했을 경우 터미널 출력
-					if success is not True:
-						print(f"쓰기 실패: {success}")
+					# 난수를 D 레지스터에 쓰기 테스트
+					try:
+						success = client_socket.mc_write_d_word(temp_read, int(temp_data * 10))
 
-						now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+						# 만약 쓰는데 실패했을 경우 터미널 출력
+						if success is not True:
+							print(f"쓰기 실패: {success}")
 
-						# D 레지스터 쓰기 이상 발생 시 로그 출력과 DB로 전달할 경고 메시지 변수
-						write_d_word_alarm_msg = (f"{now} PLC 온도 데이터 쓰기 실패: {success}")
+							now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-						# 메인 대시보드 시스템 로그 창에 D 레지스터 오류 메시지 출력 
-						self.log_signal.emit(write_d_word_alarm_msg)
+							# D 레지스터 쓰기 이상 발생 시 로그 출력과 DB로 전달할 경고 메시지 변수
+							write_d_word_alarm_msg = (f"{now} PLC 온도 데이터 쓰기 실패: {success}")
 
-						# 알람 발생 시 DB 저장
-						db_manager.insert_log(write_d_word_alarm_msg, "쓰기 오류 발생", now)
+							# 메인 대시보드 시스템 로그 창에 D 레지스터 오류 메시지 출력 
+							self.log_signal.emit(write_d_word_alarm_msg)
 
-				# 쓰는데 오류 발생한 이유 출력
-				except Exception as ex:
-					print(f"쓰기 오류 발생: {ex}")
+							# 알람 발생 시 DB 저장
+							db_manager.insert_log(write_d_word_alarm_msg, "쓰기 오류 발생", now)
+							break
 
-				# high와 low가 얼마인지 불러오기
-				high = float(MySetting.get('limit_high'))
-				low = float(MySetting.get('limit_low'))
+					# 쓰는데 오류 발생한 이유 출력
+					except Exception as ex:
+						print(f"쓰기 오류 발생: {ex}")
+						break
 
-				# 현재 온도 데이터가 상한치보다 클 경우 M100 알람 울리기
-				if temp_data > high:
-					self.m_register_connect(client_socket, address= 100, alarm= True)
-					self.m_register_connect(client_socket, address= 101, alarm= False)
+					# high와 low가 얼마인지 불러오기
+					high = float(MySetting.get('limit_high'))
+					low = float(MySetting.get('limit_low'))
 
-				# 현재 온도 데이터가 하한치보다 낮을 경우 M101 알람 울리기
-				elif temp_data < low:
-					self.m_register_connect(client_socket, address= 101, alarm= True)
-					self.m_register_connect(client_socket, address= 100, alarm= False)
+					# 현재 온도 데이터가 상한치보다 클 경우 M100 알람 울리기
+					if temp_data > high:
+						self.m_register_connect(client_socket, address= 100, alarm= True)
+						self.m_register_connect(client_socket, address= 101, alarm= False)
 
-				else:
-					self.m_register_connect(client_socket, address= 100, alarm= False)
-					self.m_register_connect(client_socket, address= 101, alarm= False)
+					# 현재 온도 데이터가 하한치보다 낮을 경우 M101 알람 울리기
+					elif temp_data < low:
+						self.m_register_connect(client_socket, address= 101, alarm= True)
+						self.m_register_connect(client_socket, address= 100, alarm= False)
 
-				# D 레지스터 읽기 테스트
-				try:
-					read_result = client_socket.mc_read_d_word(temp_read)
+					else:
+						self.m_register_connect(client_socket, address= 100, alarm= False)
+						self.m_register_connect(client_socket, address= 101, alarm= False)
 
-					# 만약 읽는데 실패했을 경우 터미널 출력
-					if not isinstance(read_result, int):
-						print(f"읽기 실패: {read_result}")
+					# D 레지스터 읽기 테스트
+					try:
+						read_result = client_socket.mc_read_d_word(temp_read)
 
-						now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+						# 만약 읽는데 실패했을 경우 터미널 출력
+						if not isinstance(read_result, int):
+							print(f"읽기 실패: {read_result}")
 
-						# D 레지스터 읽기 이상 발생 시 로그 출력과 DB로 전달할 경고 메시지 변수
-						read_d_word_alarm_msg = (f"{now} PLC 온도 데이터 읽기 실패: {read_result}")
+							now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+							# D 레지스터 읽기 이상 발생 시 로그 출력과 DB로 전달할 경고 메시지 변수
+							read_d_word_alarm_msg = (f"{now} PLC 온도 데이터 읽기 실패: {read_result}")
 						
-						# 메인 대시보드 시스템 로그 창에 D 레지스터 오류 메시지 출력 
-						self.log_signal.emit(read_d_word_alarm_msg)
+							# 메인 대시보드 시스템 로그 창에 D 레지스터 오류 메시지 출력 
+							self.log_signal.emit(read_d_word_alarm_msg)
 						
-						# 알람 발생 시 DB 저장
-						db_manager.insert_log(read_d_word_alarm_msg, "읽기 오류 발생", now)
+							# 알람 발생 시 DB 저장
+							db_manager.insert_log(read_d_word_alarm_msg, "읽기 오류 발생", now)
+							break
 
-				# 읽는데 오류 발생한 이유 출력
-				except Exception as ex:
-					print(f"읽기 오류 발생: {ex}")
+					# 읽는데 오류 발생한 이유 출력
+					except Exception as ex:
+						print(f"읽기 오류 발생: {ex}")
+						break
 
-				# 작업 스레드: 가상 온도 데이터 송신 시그널 발생 (main.py로)
-				self.temp_signal.emit(temp_data)
-				# 매초 온도 검사하여 이상 시 시그널 발생
-				self.check_temp_limit(temp_data)
+					# 작업 스레드: 가상 온도 데이터 송신 시그널 발생 (main.py로)
+					self.temp_signal.emit(temp_data)
+					# 매초 온도 검사하여 이상 시 시그널 발생
+					self.check_temp_limit(temp_data)
 
-				count += 1
-				# 입력 값이 정수 또는 실수인지 확인하기
-				if isinstance(temp_data, (int, float)):        
+					count += 1
+					# 입력 값이 정수 또는 실수인지 확인하기
+					if isinstance(temp_data, (int, float)):        
 
-					# 10초가 지났는지 확인하기
-					if count >= 10:
-						now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+						# 10초가 지났는지 확인하기
+						if count >= 10:
+							now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-						low = float(MySetting.get('limit_low'))
-						high = float(MySetting.get('limit_high'))
+							low = float(MySetting.get('limit_low'))
+							high = float(MySetting.get('limit_high'))
 
-						try:    
-							db_manager.insert_temperature(now, low, high, round(temp_data, 1))
+							try:    
+								db_manager.insert_temperature(now, low, high, round(temp_data, 1))
 
-						except Exception as ex:
-							print(f"DB 저장 실패: {ex}")
+							except Exception as ex:
+								print(f"DB 저장 실패: {ex}")
 
-						# 카운트 리셋
-						count = 0
+							# 카운트 리셋
+							count = 0
 
-				# 입력 값이 정수 또는 실수가 아니라면 아래 문구 출력
-				else:
-					print(f"[저장 실패] 잘못된 데이터 수신: 값={temp_data}, 타입={type(temp_data)}")
+					# 입력 값이 정수 또는 실수가 아니라면 아래 문구 출력
+					else:
+						print(f"[저장 실패] 잘못된 데이터 수신: 값={temp_data}, 타입={type(temp_data)}")
 
-				# 1초 반복 주기 지연
-				time.sleep(1)
+					# 1초 반복 주기 지연
+					time.sleep(1)
 
-		# 에러가 발생하여 수집이 중단되었을 경우 문구 출력, 에러의 구체적인 이유를 ex 변수에 자동으로 담아줌.
-		except Exception as ex:
-			print(f"수집을 중단합니다. 원인: {ex}")
-			self.communication_signal.emit("통신 끊김")
+			# 에러가 발생하여 수집이 중단되었을 경우 문구 출력, 에러의 구체적인 이유를 ex 변수에 자동으로 담아줌.
+			except Exception as ex:
+				# 사용자가 직접 해제 버튼을 누른 게 아니라면 에러 로그 출력
+				# (사용자 직접 해제 버튼 에러 로그는 'def communication_disconnect()'에 따로 해놨음.)
+				if self.power:
+					print(f"수집을 중단합니다. 원인: {ex}")
+					self.communication_signal.emit("통신 끊김")
 
-			now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+					now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-			# 에러가 발생하여 통신 끊김 발생 시 로그 출력과 DB로 전달할 경고 메시지 변수
-			communication_signal_alarm_msg = (f"{now} [안내] 통신 끊김: {ex}")
+					# 에러가 발생하여 통신 끊김 발생 시 로그 출력과 DB로 전달할 경고 메시지 변수
+					communication_signal_alarm_msg = (f"{now} [안내] 통신 끊김: {ex}")
 						
-			# 메인 대시보드 시스템 로그 창에 통신 끊김 오류 메시지 출력 
-			self.log_signal.emit(communication_signal_alarm_msg)
+					# 메인 대시보드 시스템 로그 창에 통신 끊김 오류 메시지 출력 
+					self.log_signal.emit(communication_signal_alarm_msg)
 						
-			# 알람 발생 시 DB 저장
-			db_manager.insert_log(communication_signal_alarm_msg, "통신 끊김 발생", now)
+					# 알람 발생 시 DB 저장
+					db_manager.insert_log(communication_signal_alarm_msg, "통신 끊김 발생", now)
 
-		finally:
-			client_socket.close()
+			# 소켓 닫고 3초 뒤에 바깥 while문으로 올라가서 재접속 시도
+			finally:
+				client_socket.close()
+
+				# 사용자가 해제 버튼을 누른게 아니라 통신 장애라면 3초 대기 후 재연결하기
+				if self.power:
+						time.sleep(3)
 
 	# 멀티쓰레드를 종료하는 메소드
 	def stop(self):
